f9124807bf15fe7465c4f1f4f310fd550f789187
Manoj Khanna <manojmkhanna@gmail.com>
2025-12-15 15:57:21 +0530

    Added Name and Address column headers selecting

--- main.py
+++ main.py
@@ -2,11 +2,62 @@ import gradio as gr
 import pandas as pd
 
 
-def excel_file_changed(excel_file):
-    if excel_file is None:
-        return None
+RESP_MAX_COUNT = 20
+ADDR_MAX_COUNT = 10
 
-    return pd.read_excel(excel_file)
+
+def excel_file_changed(excel_path):
+    if excel_path is None:
+        return [None] * (1 + RESP_MAX_COUNT + (RESP_MAX_COUNT * ADDR_MAX_COUNT))
+
+    outputs = []
+
+    excel_data_frame = pd.read_excel(excel_path)
+
+    outputs.append(excel_data_frame)
+
+    excel_column_headers = excel_data_frame.columns.tolist()
+
+    for i in range(RESP_MAX_COUNT):
+        name_dropdown = gr.Dropdown(
+            choices=excel_column_headers,
+            value=excel_column_headers[0]
+        )
+
+        outputs.append(name_dropdown)
+
+    for i in range(RESP_MAX_COUNT):
+        for j in range(ADDR_MAX_COUNT):
+            addr_dropdown = gr.Dropdown(
+                choices=excel_column_headers,
+                value=excel_column_headers[0]
+            )
+
+            outputs.append(addr_dropdown)
+
+    return outputs
+
+
+def resp_slider_changed(resp_count):
+    resp_tabs = []
+
+    for i in range(RESP_MAX_COUNT):
+        resp_tabs.append(gr.Tab(
+            visible=i < resp_count
+        ))
+
+    return resp_tabs
+
+
+def addr_slider_changed(addr_count):
+    addr_dropdowns = []
+
+    for i in range(ADDR_MAX_COUNT):
+        addr_dropdowns.append(gr.Dropdown(
+            visible=i < addr_count
+        ))
+
+    return addr_dropdowns
 
 
 with gr.Blocks(title="CNICA Excel Cleaner") as app:
@@ -24,10 +75,87 @@ with gr.Blocks(title="CNICA Excel Cleaner") as app:
         headers=[""]
     )
 
+    gr.Markdown("### Step 2: Select No. of Respondents ###")
+
+    resp_slider = gr.Slider(
+        label="No. of Respondents",
+        minimum=1,
+        maximum=RESP_MAX_COUNT,
+        step=1,
+        interactive=True
+    )
+
+    gr.Markdown("### Step 3: Select Respondent's Name and Address Column Headers ###")
+
+    resp_tabs = []
+    name_dropdowns = []
+    addr_sliders = []
+    addr_dropdowns_dict = {}
+
+    for i in range(RESP_MAX_COUNT):
+        with gr.Tab(
+            label=f"Respondent {i + 1}",
+            visible=i == 0
+        ) as resp_tab:
+            with gr.Row():
+                with gr.Column():
+                    name_dropdown = gr.Dropdown(
+                        label="Name column header",
+                        interactive=True
+                    )
+
+                    name_dropdowns.append(name_dropdown)
+
+                with gr.Column():
+                    addr_slider = gr.Slider(
+                        label="No. of Address column headers",
+                        minimum=1,
+                        maximum=ADDR_MAX_COUNT,
+                        step=1,
+                        interactive=True
+                    )
+
+                    addr_sliders.append(addr_slider)
+
+                    addr_dropdowns = []
+
+                    for j in range(ADDR_MAX_COUNT):
+                        addr_dropdown = gr.Dropdown(
+                            label=f"Address column header {j + 1}",
+                            interactive=True,
+                            visible=j == 0
+                        )
+
+                        addr_dropdowns.append(addr_dropdown)
+
+                    addr_dropdowns_dict[i] = addr_dropdowns
+
+                    addr_slider.change(
+                        fn=addr_slider_changed,
+                        inputs=addr_slider,
+                        outputs=addr_dropdowns
+                    )
+
+        resp_tabs.append(resp_tab)
+
+    resp_slider.change(
+        fn=resp_slider_changed,
+        inputs=resp_slider,
+        outputs=resp_tabs
+    )
+
+    excel_file_changed_outputs = []
+    excel_file_changed_outputs.append(excel_data_frame)
+    for i in range(RESP_MAX_COUNT):
+        excel_file_changed_outputs.append(name_dropdowns[i])
+    for i in range(RESP_MAX_COUNT):
+        for j in range(ADDR_MAX_COUNT):
+            excel_file_changed_outputs.append(addr_dropdowns_dict[i][j])
+
     excel_file.change(
         fn=excel_file_changed,
         inputs=excel_file,
-        outputs=excel_data_frame
+        outputs=excel_file_changed_outputs
     )
 
 app.launch()
