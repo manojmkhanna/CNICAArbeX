d6f959c8b49fd7b737a14a6eb98e56d336b08e6a
Manoj Khanna <manojmkhanna@gmail.com>
2025-12-15 17:57:42 +0530

    Added auto selection of Address column headers

--- main.py
+++ main.py
@@ -6,16 +6,22 @@ RESP_MAX_COUNT = 20
 ADDR_MAX_COUNT = 10
 
 
+excel_data_frame = None
+excel_column_headers = None
+
+
 def excel_file_changed(excel_path):
     if excel_path is None:
         return [None] * (1 + RESP_MAX_COUNT + (RESP_MAX_COUNT * ADDR_MAX_COUNT))
 
     outputs = []
 
+    global excel_data_frame
     excel_data_frame = pd.read_excel(excel_path)
 
     outputs.append(excel_data_frame)
 
+    global excel_column_headers
     excel_column_headers = excel_data_frame.columns.tolist()
 
     for i in range(RESP_MAX_COUNT):
@@ -55,6 +61,22 @@ def addr_slider_changed(addr_count):
     for i in range(ADDR_MAX_COUNT):
         addr_dropdowns.append(gr.Dropdown(
             visible=i < addr_count
+        ))
+
+    return addr_dropdowns
+
+
+def addr_dropdown_changed(addr_column_header):
+    if addr_column_header is None:
+        return [None] * (ADDR_MAX_COUNT - 1)
+
+    addr_column_index = excel_column_headers.index(addr_column_header)
+
+    addr_dropdowns = []
+
+    for i in range(1, ADDR_MAX_COUNT):
+        addr_dropdowns.append(gr.Dropdown(
+            value=excel_column_headers[addr_column_index + i]
         ))
 
     return addr_dropdowns
@@ -134,6 +156,12 @@ with gr.Blocks(title="CNICA Excel Cleaner") as app:
                         fn=addr_slider_changed,
                         inputs=addr_slider,
                         outputs=addr_dropdowns
+                    )
+
+                    addr_dropdowns[0].change(
+                        fn=addr_dropdown_changed,
+                        inputs=addr_dropdowns[0],
+                        outputs=addr_dropdowns[1:]
                     )
 
         resp_tabs.append(resp_tab)
